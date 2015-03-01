d1a9bd38e58ccad758ce50cf56f44263ef275e55
tiffanym <huntergirl2015@gmail.com>
2015-03-01 14:05:04 -0500

    Issue popped up when trying to use arraylist.... problems/ errors listed with '//***ISSUE' next to them (sorta got lazy and just highlighted what I thought seemed to be the issues based on scanning what appeared in the terminal)

--- 04USACO/CowsTravel.java
+++ 04USACO/CowsTravel.java
@@ -1,4 +1,6 @@
 //For Question 7 "Cow Travelling" from http://tjsct.wikidot.com/usaco-mar08-silver
+import java.util.*;
+
 public class CowsTravel{
     String[][] pasture;
     int N,M,T; //N=row(2<=N<=100) //M=col(2<=M<=100) //T=time(0<T<=15)
@@ -34,23 +36,50 @@ public class CowsTravel{
 		C2=Integer.parseInt(startEnd[3]);
 	    }
 	}
+	pathsR.add(new int[T]);//*** ISSUE
+	pathsC.add(new int[T]);//*** ISSUE
     }
 
-    public void FJneedstoKnow(){
+    public int FJneedstoKnow(){
 	//startR,startC= (R1-1,C1-1); endR,endC=(R2-1,C2-1);
-	travel(R1-1,C1-1,1);
+	if (travel(R1-1,C1-1,1)){
+	    return S;
+	}else{
+	    return 0;
+	}
     }
 
     public boolean travel(int row,int col,int time){
-	if (row==R2-1 && col==C2-1 && time==T){
-	    pathsR.get(S).set(T-1,row);
-	    pathsC.get(S).set(T-1,col);
-	    return true;
-	}
-	if (pasture[row][col]!='.'){ //is a *(tree) or some labeled number
+	if (row<0 || row>=N || col<0 || col>=M || time<0 || time>T) { //out of bounds
 	    return false;
 	}
-	//then do something here
+	if (row==R2-1 && col==C2-1 && time==T){ //finished / at end position
+	    //pathsR.get(S).set(time-1,""+row);
+	    //pathsC.get(S).set(time-1,""+col);
+	    pathsR.get(S).add(""+row);      //*** ISSUE
+	    pathsC.get(S).add(""+col);	    //*** ISSUE
+	    S+=1;
+	    //pathsR.add(new ArrayList<String>());
+	    //pathsC.add(new ArrayList<String>());
+	    pathsR.add(new int[T]);         //*** ISSUE
+	    pathsC.add(new int[T]);         //*** ISSUE
+	    return true;
+	}
+	if (pasture[row][col]!=""+'.'){ //is a '*'(tree) or some labeled number
+	    return false;
+	}
+	//Can a path be made from the current position with the given amount of steps/ time?
+	if (travel(row+1,col,time+1) || travel(row,col+1,time+1) ||
+	    travel(row-1,col,time+1)|| travel(row,col-1,time+1)){
+	    if(!(Integer.parseInt(pathsR.get(S).get(time-1))>0) &&
+	       !(Integer.parseInt(pathsC.get(S).get(time-1))>0)){
+		//pathsR.get(S).set(time-1,""+row);
+		//pathsC.get(S).set(time-1,""+col);
+		pathsR.get(S).add(""+row);
+		pathsC.get(S).add(""+col);
+		return true;
+	    }
+	}
     }
     
     public static void main(String[] args){
