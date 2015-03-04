e216f9fa73dd36fce152b6af66f5447bd05f8843
tiffanym <huntergirl2015@gmail.com>
2015-03-03 22:25:47 -0500

    It works! hehe muahaha... I'm so tired right now x.x

--- 04USACO/CowsTravel.java
+++ 04USACO/CowsTravel.java
@@ -1,13 +1,13 @@
 //For Question 7 "Cow Travelling" from http://tjsct.wikidot.com/usaco-mar08-silver
 import java.util.*;
+import java.io.*;
 
 public class CowsTravel{
     String[][] pasture;
     int N,M,T; //N=row(2<=N<=100) //M=col(2<=M<=100) //T=time(0<T<=15)
     int R1,C1,R2,C2; //(R1,C1)=start (R2,C2)=end
-    int S=0; //number of ways cows can travel with given info
-    ArrayList<String> paths=new ArrayList<String>();
-
+    int ways=0;
+    /*
     public CowsTravel(String input){
 	String[] info=input.split("\n");
 	for (int line=0;line<info.length;line++){
@@ -35,54 +35,70 @@ public class CowsTravel{
 		C2=Integer.parseInt(startEnd[3]);
 	    }
 	}
-	pathsR.add(new int[T]);//*** ISSUE
-	pathsC.add(new int[T]);//*** ISSUE
+	//paths.add();
+    }
+    */
+    public CowsTravel(){
+	File info=new File("ctravel.in");
+	try{
+	    Scanner scan= new Scanner(info);
+	    int line=0;
+	    while (scan.hasNextLine()){
+		String text=scan.nextLine();
+		//sets variables
+		if (line==0){
+		    String[] para=text.split(" ");
+		    N=Integer.parseInt(para[0]);
+		    M=Integer.parseInt(para[1]);
+		    T=Integer.parseInt(para[2]);
+		    pasture= new String[N][M];
+		}
+		//sets pasture
+		if (line>0 && line<N+1){
+		    String[] pastureRow=text.split("");
+		    for (int col=0;col<M;col++){
+			pasture[line-1][col]=pastureRow[col];
+		    }
+		}
+		//sets start and stop coordinates
+		if (line==N+1){
+		    String[] startEnd=text.split(" ");
+		    R1=Integer.parseInt(startEnd[0]);
+		    C1=Integer.parseInt(startEnd[1]);
+		    R2=Integer.parseInt(startEnd[2]);
+		    C2=Integer.parseInt(startEnd[3]);
+		}
+		line++;
+	    }
+	}catch (FileNotFoundException e){
+
+	}
     }
 
     public int FJneedstoKnow(){
 	//startR,startC= (R1-1,C1-1); endR,endC=(R2-1,C2-1);
-	if (travel(R1-1,C1-1,1)){
-	    return S;
-	}else{
-	    return 0;
-	}
+	return travel(R1-1,C1-1,1);
     }
 
-    public boolean travel(int row,int col,int time){
+    public int travel(int row,int col,int time){
 	if (row<0 || row>=N || col<0 || col>=M || time<0 || time>T) { //out of bounds
-	    return false;
+	    return 0;
 	}
-	if (row==R2-1 && col==C2-1 && time==T){ //finished / at end position
-	    //pathsR.get(S).set(time-1,""+row);
-	    //pathsC.get(S).set(time-1,""+col);
-	    pathsR.get(S).add(""+row);      //*** ISSUE
-	    pathsC.get(S).add(""+col);	    //*** ISSUE
-	    S+=1;
-	    //pathsR.add(new ArrayList<String>());
-	    //pathsC.add(new ArrayList<String>());
-	    pathsR.add(new int[T]);         //*** ISSUE
-	    pathsC.add(new int[T]);         //*** ISSUE
-	    return true;
+	if (time==T && row==R2-1 && col==C2-1){ //finished / at end position
+	    return 1;
 	}
-	if (pasture[row][col]!=""+'.'){ //is a '*'(tree) or some labeled number
-	    return false;
+	if (pasture[row][col]==""+'*'){ //is a '*'(tree)
+	    return 0;
 	}
 	//Can a path be made from the current position with the given amount of steps/ time?
-	if (travel(row+1,col,time+1) || travel(row,col+1,time+1) ||
-	    travel(row-1,col,time+1)|| travel(row,col-1,time+1)){
-	    if(!(Integer.parseInt(pathsR.get(S).get(time-1))>0) &&
-	       !(Integer.parseInt(pathsC.get(S).get(time-1))>0)){
-		//pathsR.get(S).set(time-1,""+row);
-		//pathsC.get(S).set(time-1,""+col);
-		pathsR.get(S).add(""+row);
-		pathsC.get(S).add(""+col);
-		return true;
-	    }
-	}
+	return travel(row+1,col,time+1) + travel(row,col+1,time+1) +
+		travel(row-1,col,time+1) + travel(row,col-1,time+1);
     }
     
     public static void main(String[] args){
-	String input="4 5 6\n...*.\n...*.\n.....\n.....\n1 3 1 5";
-	CowsTravel test=new CowsTravel(input);
+	//String input="4 5 6\n...*.\n...*.\n.....\n.....\n1 3 1 5";
+	//CowsTravel test=new CowsTravel(input);
+	CowsTravel test=new CowsTravel();
+	System.out.println(test.FJneedstoKnow());
     }
 }
