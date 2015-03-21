02b3c53b5a4f9c61644ecf89ab0dbea21f318d59
tiffanym <huntergirl2015@gmail.com>
2015-03-21 10:25:51 -0400

    added peek method with test cases and added to String to see that code was pushing properly; works so far with the test cases I've used (tried to account for exceptions in test cases, but there may be some things I didn't think of while testing my code

--- 09Stacks/MyStack.java
+++ 09Stacks/MyStack.java
@@ -36,15 +36,41 @@ public class MyStack<T>{
 	return out;
     }
 
+    public T peek(){
+	if (isEmpty()){
+	    throw new EmptyStackException();
+	}
+	T out=head.getValue();
+	return out;
+    }
+
+    //for testing purposes
+    public String toString(){
+	String ans="[ ";
+	LNode<T> current=head;
+	while (current!=null){
+	    ans+=current.getValue()+" ";
+	    current=current.getNext();
+	}
+	ans+="]";
+	return ans;
+    }
+
     public static void main(String[] args){
 	MyStack<Integer> test=new MyStack<Integer>();
 	System.out.println(test.push(0));
 	System.out.println(test.push(-5));
+	System.out.println(test.toString());
 	//System.out.println(test.push(3));
 	//System.out.println(test.push(4));
-	System.out.println(test.pop());
-	System.out.println(test.pop());
-	System.out.println(test.pop());
+	//System.out.println(test.pop());
+	//System.out.println(test.pop());
+	//System.out.println(test.pop());
+	System.out.println(test.peek());
+	System.out.println(test.toString());
+	System.out.println(test.push(3));
+	System.out.println(test.toString());
+	System.out.println(test.peek());
 	
 	
     }
