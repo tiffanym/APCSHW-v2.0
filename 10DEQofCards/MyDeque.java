fc514a7e3ee601774c2d527f960294c976b1e502
tiffanym <huntergirl2015@gmail.com>
2015-03-25 12:40:07 -0400

    created new file for Deque

---
+++ 10DEQofCards/MyDeque.java
@@ -0,0 +1,34 @@
+public class MyDeque<T>{
+    T[] data;
+    int head,tail;
+
+    public void addFirst(T value){
+	
+    }
+
+    public void addLast(T value){
+	if (t==data.length){
+	    
+	}
+    }
+    
+    public T removeFirst(){
+
+    }
+
+    public T removeLast(){
+
+    }
+
+    public T getFirst(){
+
+    }
+
+    public T getLast(){
+
+    }
+
+    public T[] resize(){
+	
+    }
+}
