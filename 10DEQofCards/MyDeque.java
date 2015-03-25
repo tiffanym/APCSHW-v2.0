a1cbdad12dc173f65d88bc85b2e84c693dbfc3a0
tiffanym <huntergirl2015@gmail.com>
2015-03-25 12:50:17 -0400

    added resize function

--- 10DEQofCards/MyDeque.java
+++ 10DEQofCards/MyDeque.java
@@ -36,11 +36,24 @@ public class MyDeque<T>{
 	return data[data.length-1];
     }
 
-    public T[] resize(){
-	
+    public T[] resize(int size){
+	T[] out=new T[size];
+	if (size==data.length){
+	    return data;
+	}else if (size<data.length){
+	    for (int i=0;i<size;i++){
+		out[i]=data[i];
+	    }
+	}else{ //size>data.length
+	    for (int i=0;i<data.length;i++){
+		out[i]=data[i];
+	    }
+	}
+	return out;
     }
     
     public static void main(String[] args){
+	MyDeque test=new MyDeque();
 	
     }
 }
