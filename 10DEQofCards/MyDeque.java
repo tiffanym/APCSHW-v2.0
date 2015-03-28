96a3e014e58f939ce5ae3dd29ff25a24de31248e
tiffanym <huntergirl2015@gmail.com>
2015-03-27 20:33:01 -0400

    still editing resize and add methods

--- 10DEQofCards/MyDeque.java
+++ 10DEQofCards/MyDeque.java
@@ -5,25 +5,30 @@ public class MyDeque<T>{
     int size;
 
     public MyDeque(){
+	data=new T[10];
 	head=0;
 	tail=0;
 	size=0;
     }
-
+    //h--,insert
     public void addFirst(T value){
-	if (head==data.length){
-	    resize(data.length+1); //change so that everything moves forward and can add to front?
+	resize();
+	if (head<0){
+	    head+=data.length;
 	}
-	data[head]==value;
-	head++;
+	data[head]=value;
+	head--;
+	size++;
     }
-
+    //t++,insert
     public void addLast(T value){
+	resize();
 	if (tail==data.length){
-	    resize(data.length+1);
+	    tail=tail%data.length;
 	}
 	data[tail]=value;
 	tail++;	
+	size++;
     }
     
     public T removeFirst(){
@@ -58,14 +63,25 @@ public class MyDeque<T>{
 	int temp=data.length;
 	if (size==data.length){
 	    temp=2*data.length;
-	}else if (size<(int)(0.25*data.length)){
-	    temp=(int)(0.5*data.length);
 	}
 	T out=new T[temp];
-	for (int i=0;i<data.length && i<temp;i++){
-	    out[i]=data[i];
+	if (head<=tail){
+	    for (int i=head;i<tail+1;i++){
+		out[i]=data[i];
+	    }
+	}else{ //tail<head
+	    for (int i=0;i<tail;i++){
+		out[i]=data[i];
+	    }
+	    for (int i=head;head<data.length;i++){
+		out[i]=data[i];
+	    }
 	}
 	data=out;
+	head=0;
+	if (size>0){
+	    tail=size-1;
+	}
     }
     
     public static void main(String[] args){
