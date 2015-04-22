3f6761fe6bdba0c1871e6c6c24223786186bb2a2
tiffanym <huntergirl2015@gmail.com>
2015-04-22 13:04:00 -0400

    added toString method to TreeNode

--- TreeNode.java
+++ TreeNode.java
@@ -1,9 +1,22 @@
 public class TreeNode<T>{
     T data;
     TreeNode<T> left,right;
+    private static final int preORDER=0;
+    private static final int postORDER=1;
+    private static final int inORDER=2;
 
     public TreeNode(){
 
+    }
+
+    public String toString(int order){
+	if (order==preORDER){//V left right
+
+	}else if(order==postORDER){// left right V
+
+	}else if(order==inORDER){// left V right
+
+	}	
     }
 
 }
