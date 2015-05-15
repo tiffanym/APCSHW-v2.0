d5b4832897ef76b22237499bf49978a07a11147e
tiffanym <huntergirl2015@gmail.com>
2015-05-15 18:51:35 -0400

    added Sorts file and skeleton of methods; no actual code yet

---
+++ 13Heap/Sorts.java
@@ -0,0 +1,17 @@
+public class Sorts{
+    int[] ary;
+
+    public Sorts(){
+
+    }
+
+    public void heapsort(int[] ary){
+	heapify(ary);
+	//loop
+	////remove and place at end
+    }
+
+    public static void main(String[] args){
+	Sorts test = new Sorts();
+    }
+}
