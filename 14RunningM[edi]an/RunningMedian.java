753299b0eff517224919c157cacdddbd8fdef0f9
tiffanym <huntergirl2015@gmail.com>
2015-05-14 22:06:37 -0400

    commented out debug statments and seems like it's working

--- 14RunningM[edi]an/RunningMedian.java
+++ 14RunningM[edi]an/RunningMedian.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class RunningMedian{
     MyHeap maxHeap; //holds everything less than the median
     MyHeap minHeap; //holds everything greater than the median
@@ -13,18 +14,24 @@ public class RunningMedian{
 	    maxHeap.add(eger);
 	}else{
 	    resize();
-	    if (eger<maxHeap.peek()){
+	    //System.out.println("MaxHeap size: "+maxHeap.size());
+	    //System.out.println("MinHeap size: "+minHeap.size());
+	    if (maxHeap.size()==0 || eger<maxHeap.peek()){
+		//System.out.println("Adding to maxHeap");
 		maxHeap.add(eger);
-	    }else{
+	    }else if ( minHeap.size()==0 || eger>=minHeap.peek()){
+		//System.out.println("Adding to minHeap");
 		minHeap.add(eger);
 	    }
 	}
     }
 
     public double getMedian(){
+	resize();
 	double median;
 	if (maxHeap.size()==minHeap.size()){
-	    median = (double)(maxHeap.peek() + minHeap.size())/2.0;
+	    //System.out.println("Median is avg of "+maxHeap.peek()+" and "+minHeap.peek());
+	    median = (double)(maxHeap.peek() + minHeap.peek())/2.0;
 	}else{
 	    if (maxHeap.size()>minHeap.size()){
 		median = (double)(maxHeap.peek());
@@ -38,15 +45,40 @@ public class RunningMedian{
     public void resize(){
 	if (Math.abs(maxHeap.size()-minHeap.size())>1){
 	    if (maxHeap.size()>minHeap.size()){
-		while (maxHeap.size()-1 != minHeap.size()){
+		while (maxHeap.size()-1 > minHeap.size()){
 		    minHeap.add(maxHeap.remove());
 		}
 	    }else if (minHeap.size()>maxHeap.size()){
-		while (minHeap.size()-1 != maxHeap.size()){
+		while (minHeap.size()-1 > maxHeap.size()){
 		    maxHeap.add(minHeap.remove());
 		}
 	    }
 	}
+    }
+
+
+    //for testing purposes
+    public String toString(){
+	resize();
+	System.out.print("Max heap: ");
+	System.out.println(maxHeap.toString());
+	System.out.println();
+	System.out.print("Min heap: ");
+	System.out.println(minHeap.toString());
+	System.out.println();
+	System.out.println("Median: "+getMedian());
+	return "";
+    }
+    public static void main(String[] args){
+	RunningMedian test=new RunningMedian();
+	Random r = new Random();
+	test.add(4);
+	//for (int i=0;i<1;i++){
+	//    test.add(r.nextInt(500));
+	//}
+	test.add(42);
+	test.add(2);
+	test.toString();
     }
 
 }
